430a14225d5f92eb555cb40d99a1e30efee6d543
unknown <harshgoel2004.work@gmail.com>
2026-01-02 02:02:14 +0530

    added color picker

---
+++ components/ui/color-picker.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import React from 'react'
+
+const ColorPicker = () => {
+  return (
+    <div>
+        Color Picker
+        <div>
+            <input type="color" />
+        </div>
+    </div>
+  )
+}
+
+export default ColorPicker
